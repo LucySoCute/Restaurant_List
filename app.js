e3a2e237eb2b6d352ffc82bbe2496e8bbe2ab463
LucySoCute <cutelucysu@gmail.com>
2022-10-02 13:21:41 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const restaurantList = require('./restaurant.json')
+const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+//show restaurant
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+//search restaurants
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase().trim()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
